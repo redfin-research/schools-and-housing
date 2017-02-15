4955eb7b12058191d057ff0a1aa81c600e49791e
redfin-research <research.team@redfin.com>
2017-02-15 15:25:46 -0800

    Add files via upload

--- preferences_layer/data_merging.r
+++ preferences_layer/data_merging.r
@@ -15,12 +15,13 @@ packagesMissing <- packagesRequired[!(packagesRequired %in% installed.packages()
 for (package in packagesMissing) {
     install.packages(package, repos='http://cran.rstudio.com/')
 }
+
 for (package in packagesRequired){
     eval(parse(text=paste0('library(', package, ')')))
 }
 
 # Pull population density and average commute times for each city
-geo <- geo.make(state="*", place = "*")
+geo <- geo.make(zip.code = "*")
 
 travel_times <- acs.fetch(endyear = 2015, span = 5, geography = geo,
                           table.number = "B08303", col.names = "pretty")
@@ -30,9 +31,8 @@ travel_times_df <- data.frame(travel_times@geography,
                               travel_times@estimate, 
                               stringsAsFactors = FALSE)
 
-names(travel_times_df) <- c('place', 
-                            'state_fips', 
-                            'place_fips', 
+names(travel_times_df) <- c('zip_name', 
+                            'zip_codes', 
                             'total_commuters',
                             'commute_Less_than_5_minutes',
                             'commute_5_to_9_minutes',
@@ -47,12 +47,14 @@ names(travel_times_df) <- c('place',
                             'commute_60_to_89_minutes',
                             'commute_90_or_more_minutes')
 
+row.names(travel_times_df) <- 1:nrow(travel_times_df)
+
 travel_times_df <- travel_times_df %>%
-    mutate(fips = str_c(str_pad(state_fips, 2, "left", "0"), str_pad(place_fips, 5, "left", "0")),
-           commute_under_30 = commute_Less_than_5_minutes + commute_5_to_9_minutes + 
+    mutate(commute_under_30 = commute_Less_than_5_minutes + commute_5_to_9_minutes + 
                commute_10_to_14_minutes + commute_15_to_19_minutes + commute_20_to_24_minutes + commute_25_to_29_minutes,
            commute_greater_than_30 = total_commuters - commute_under_30,
-           percent_under_30 = commute_under_30 / total_commuters)
+           percent_under_30 = commute_under_30 / total_commuters) %>%
+    select(-zip_name)
 
 glimpse(travel_times_df)
 
@@ -65,24 +67,21 @@ population_df <- data.frame(population@geography,
                             population@estimate, 
                               stringsAsFactors = FALSE)
 
-names(population_df) <- c('place', 
-                            'state_fips', 
-                            'place_fips', 
+names(population_df) <- c('zip_name', 
+                            'zip_codes', 
                             'total_population')
 
 
+row.names(population_df) <- 1:nrow(population_df)
 
-population_df <- population_df %>%
-    mutate(fips = str_c(str_pad(state_fips, 2, "left", "0"), str_pad(place_fips, 5, "left", "0"))) %>%
-    select(fips, total_population)
 
 glimpse(population_df)
 
 # combine these two data frames
-travel_times_df <- merge(travel_times_df, population_df, by = "fips")
+travel_times_df <- merge(travel_times_df, select(population_df, -zip_name), by = "zip_codes")
 glimpse(travel_times_df)
 
-# Grab Latest population data
+# Grab commuting data on percent that drive alone
 commute <- acs.fetch(endyear = 2015, span = 5, geography = geo,
                         table.number = "B08301", col.names = "pretty")
 
@@ -91,42 +90,53 @@ commute_df <- data.frame(commute@geography,
                             commute@estimate, 
                             stringsAsFactors = FALSE)
 
-commute_df <- commute_df[,c(1:4,6)]
+commute_df <- commute_df[,c(2,3,5)]
 
-names(commute_df) <- c('place', 
-                          'state_fips', 
-                          'place_fips', 
+names(commute_df) <- c('zip_codes', 
                           'total_transit',
                           'total_drive_alone')
 
+row.names(commute_df) <- 1:nrow(commute_df)
 
 commute_df <- commute_df %>%
-    mutate(fips = str_c(str_pad(state_fips, 2, "left", "0"), str_pad(place_fips, 5, "left", "0")),
-           percent_drive_alone = total_drive_alone / total_transit) %>%
-    select(fips, percent_drive_alone)
+    mutate(percent_drive_alone = total_drive_alone / total_transit) %>%
+    select(zip_codes, percent_drive_alone)
 
 glimpse(commute_df)
 glimpse(travel_times_df)
 
 travel_times_df <- merge(travel_times_df, commute_df)
 
-fips_codes <- read_csv('./preferences_layer/ny_sf_mapping.csv')
-glimpse(fips_codes)
-fips_codes$place_fips <- str_pad(fips_codes$place_fips, 7, "left", "0")
+zip_codes <- read_csv('./preferences_layer/ny_sf_mapping.csv')
+glimpse(zip_codes)
 
-combined_data <- merge(fips_codes, select(travel_times_df, -place_fips), by.x="place_fips", by.y="fips")
+combined_data <- merge(zip_codes, travel_times_df, 
+                       by.x="zip_code", by.y="zip_codes")
 
 glimpse(combined_data)
 
 combined_data <- combined_data %>% 
     mutate(pop_density = total_population / polygon_area) %>%
     arrange(desc(pop_density)) %>%
-    select(place_fips, percent_under_30, percent_drive_alone, total_population, pop_density, walk_score, polygon_area)
+    select(percent_under_30, percent_drive_alone, total_population, pop_density, walk_score, polygon_area)
 
+# rename zip_code to zipcode for join
+names(combined_data) <- c('zipcode', names(combined_data[,2:ncol(combined_data)]))
+
+# full_dataset is the final dataset from the school_zone script
 glimpse(full_dataset)
 
-all_data_sf_ny <- merge(combined_data, full_dataset, by = "place_fips")
+full_dataset_ny_sf <- filter(full_dataset, 
+       metropolitan_statistical_area %in% 
+           c('San Francisco-Oakland-Hayward, CA', 'New York-Newark-Jersey City, NY-NJ-PA'))
 
+glimpse(full_dataset_ny_sf)
+
+all_data_sf_ny <- right_join(combined_data, full_dataset_ny_sf, by = 'zipcode')
+
+filter(all_data_sf_ny, is.na(total_population))
 
 write_csv(all_data_sf_ny, './preferences_layer/all_data_ny_sf.csv')
 
+# in case we need to map each zip to a place_fips
+# zip_fips_mapping <- read_csv('http://www2.census.gov/geo/docs/maps-data/data/rel/zcta_place_rel_10.txt')
--- preferences_layer/ny_sf_mapping.sql
+++ preferences_layer/ny_sf_mapping.sql
@@ -1,29 +1,10 @@
-with pp as (
-	select place_id
-	, sum(total_population) as total_population
-	from places_population
-	group by 1
-)
-, p2 as (
-	select distinct place_id
-	, first_value(id2) over(partition by place_id order by total_population desc) as place_fips
-	from places_population
-), 
-principal_city_suburbs as ( --identify suburbs
-select pp.place_id
-, p2.place_fips
-, p.polygon_area
-, (case 
-	when dense_rank() over (partition by m.cbsa_title order by pp.total_population desc) = 1 
-	then 'principal_city' else 'suburb' end) as is_suburb
-from places p
-join geo_metro m on p.county_id = m.county_id
-join pp on pp.place_id = p.place_id
-join p2 on pp.place_id = p2.place_id
-where cbsa_title in ('San Francisco-Oakland-Hayward, CA', 'New York-Newark-Jersey City, NY-NJ-PA')
-)
-select p.*, w.walk_score 
-from principal_city_suburbs p
-left join region_walkscores w on p.place_id = w.region_table_id 
-	and w.region_type_id = 6
-;
+--Grab Walk score data by zip code
+select zip_code, z.polygon_area, cbsa_title, w.walk_score
+from zip_codes z
+join geo_metro m on z.county_id = m.county_id
+join region_view v on v.display_name = z.zip_code
+left join region_walkscores w on v.table_id = w.region_table_id and w.region_type_id = 2
+where cbsa_title in (
+	'San Francisco-Oakland-Hayward, CA', 
+	'New York-Newark-Jersey City, NY-NJ-PA'
+);
